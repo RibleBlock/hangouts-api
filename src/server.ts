451c18a48d1874c799de4853d1164c70cdcb5420
Riquelme <riquelmeaparecido08@gmail.com>
2022-08-15 09:22:08 -0300

    netlify e vercel funciona

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
-const origin = 'https://hangoutspizzaria.vercel.app';
 const port = process.env.PORT || 3333;
+const origin = ['https://hangoutspizzaria.vercel.app',
+  'https://hangoutspizza.netlify.app',
+  'http://localhost:3000'];
 
 const app = express();
 
-app.use(cors({ origin }));
+app.use(cors({ origin, optionsSuccessStatus: 200 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
